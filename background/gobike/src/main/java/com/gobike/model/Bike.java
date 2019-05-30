9dee01a8ee260fad35afc838579b9da06f64edc3
xssdpgy <2234501359@qq.com>
2019-05-30 23:57:19 +0800

    集成lombok，简化get/set方法

--- background/gobike/src/main/java/com/gobike/model/Bike.java
+++ background/gobike/src/main/java/com/gobike/model/Bike.java
@@ -1,5 +1,7 @@
 package com.gobike.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexType;
 import org.springframework.data.mongodb.core.index.GeoSpatialIndexed;
@@ -7,48 +9,16 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "bikes")
+@Getter
+@Setter
 public class Bike {
     @Id
     private String id;
-
     // 表示经纬度的数组[经度, 纬度]
     @GeoSpatialIndexed(type = GeoSpatialIndexType.GEO_2DSPHERE)
     private double[] location;
-
     @Indexed
     private Long bikeNo;
-
     private int status;
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public double[] getLocation() {
-        return location;
-    }
-
-    public void setLocation(double[] location) {
-        this.location = location;
-    }
-
-    public Long getBikeNo() {
-        return bikeNo;
-    }
-
-    public void setBikeNo(Long bikeNo) {
-        this.bikeNo = bikeNo;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
 }
--- background/gobike/src/main/java/com/gobike/model/User.java
+++ background/gobike/src/main/java/com/gobike/model/User.java
@@ -1,5 +1,7 @@
 package com.gobike.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -7,10 +9,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 
 @Document(collection = "users")
+@Getter
+@Setter
 public class User {
     @Id
     private String id;
-
     @Indexed
     private String phoneNum;
     private Double deposit;
@@ -19,68 +22,4 @@ public class User {
     private String nickName;
     private String name;
     private String idNum;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getPhoneNum() {
-        return phoneNum;
-    }
-
-    public void setPhoneNum(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
-
-    public Double getDeposit() {
-        return deposit;
-    }
-
-    public void setDeposit(Double deposit) {
-        this.deposit = deposit;
-    }
-
-    public Integer getStatus() {
-        return status;
-    }
-
-    public void setStatus(Integer status) {
-        this.status = status;
-    }
-
-    public Date getRegDate() {
-        return regDate;
-    }
-
-    public void setRegDate(Date regDate) {
-        this.regDate = regDate;
-    }
-
-    public String getNickName() {
-        return nickName;
-    }
-
-    public void setNickName(String nickName) {
-        this.nickName = nickName;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getIdNum() {
-        return idNum;
-    }
-
-    public void setIdNum(String idNum) {
-        this.idNum = idNum;
-    }
 }
